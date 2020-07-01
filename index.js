c2248cd7ca52b46fb4322c6451d4b9e38d6af03d
Philip 'digitalfox' Kovac <pkovac@binaryden.net>
2020-07-01 17:34:29 -0400

    Add some actual CRUD logic.

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const fastify = require("fastify");
 const mustache = require("mustache");
 const level = require("level");
 const { promisify } = require("util");
+const fs = require("fs");
+
+require.extensions[".html"] = function (module, filename) {
+  module.exports = fs.readFileSync(filename, "utf8");
+};
+
+const manageTemplate = require("./manage.html");
 // TODO: Add EC support to jwks-rsa
 const jwksEc = require("jwks-ec");
 require("dotenv").config();
@@ -13,12 +20,12 @@ const config = {
   host: process.env[`${PREFIX}_HOST`] || "0.0.0.0",
   dbPath: process.env[`${PREFIX}_DB_PATH`] || "/vixlink.json",
   jwksUri: process.env[`${PREFIX}_JWKS_URI`],
-  jwtHeader: process.env[`${PREFIX}_JWT_HEADER`] || "X-Pomerium-Jwt-Assertion",
-  trustProxy: process.env[`${PREFIX}_TRUST_PROXY`] || false,
+  jwtHeader: process.env[`${PREFIX}_JWT_HEADER`],
 };
 let db;
 let jwksClient;
 const app = fastify();
+app.register(require("fastify-formbody"));
 
 const initialize = async () => {
   try {
@@ -36,10 +43,12 @@ const initialize = async () => {
   }
 };
 
-app.get("/", async (req, res) => {
+app.addHook("onRequest", async (req, res) => {
   const token = req.headers[config.jwtHeader.toLowerCase()];
+  let h;
   // Note to self, file bug with pomerium for not attaching the key ID to the token.
   const getKey = (header, callback) => {
+    h = header;
     jwksClient.getSigningKeys((err, keys) => {
       // HAAAAACK, should read kid from header and do getSigningKey.
       const key = keys[0];
@@ -48,11 +57,54 @@ app.get("/", async (req, res) => {
   };
 
   const verifyPromise = promisify((t, k, cb) => jwt.verify(t, k, cb));
-  const authInfo = await verifyPromise(token, getKey);
-  res.send(authInfo);
+  req.jwt = await verifyPromise(token, getKey);
+
+  return;
 });
 
-app.use(`/*`, (req, res) => {});
+const getLinks = async () => {
+  return new Promise((resolve, reject) => {
+    const links = [];
+    db.createReadStream()
+      .on("data", (data) => {
+        links.push({ name: data.key, url: data.value });
+      })
+      .on("error", (err) => {
+        reject(err);
+      })
+      .on("end", (err) => {
+        resolve(links);
+      });
+  });
+};
+
+app.get("/", async (req, res) => {
+  res
+    .header("Content-Type", "text/html; charset=UTF-8")
+    .send(mustache.render(manageTemplate, { links: await getLinks() }));
+  if (!req.jwt.email) {
+    res.status(403).send();
+  }
+});
+
+app.post("/", async (req, res) => {
+  if (!req.jwt.email) {
+    res.status(403).send();
+  }
+  if (req.body.action == "delete") {
+    const name = req.body.name;
+    const url = req.body.url;
+    await db.del(req.body.name);
+  } else if (req.body.action == "create") {
+    await db.put(req.body.name, req.body.url);
+  }
+
+  res.redirect("/");
+});
+
+app.get(`/*`, (req, res) => {
+  res.send(req.raw.url);
+});
 
 initialize().then(() => {
   app.listen(Number(config.port), config.host, () =>
